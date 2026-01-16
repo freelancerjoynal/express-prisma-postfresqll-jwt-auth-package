a92ba3146652e90b4bedc5a8bc409940f78647f9
freelancerjoynal <developerjoynal@gmail.com>
2026-01-16 16:47:37 +0600

    multi role added

--- src/routes/profile/profileRouter.ts
+++ src/routes/profile/profileRouter.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { authenticated  } from '../../middleware/authMiddleware.js';
+import { authorize } from '../../middleware/roleMiddleware.js';
+
+
 
 const profileRouter = express.Router();
 
-profileRouter.get('/', authenticated, (req, res) => {
+profileRouter.get('/', authenticated, authorize('USER'), (req, res) => {
   // Accessing user data attached by the authenticated middleware
   res.json({ 
     message: 'Authenticated data accessed successfully', 
